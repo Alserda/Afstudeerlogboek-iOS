babcdd40a5631f7dee3e33067cae7113849a84f4
Peter Alserda <peteralserda@hotmail.com>
2016-03-04 14:56:13 +0100

    Constraints added to logbook

--- SAAAAA/LogbookViewController.swift
+++ SAAAAA/LogbookViewController.swift
@@ -12,6 +12,7 @@ class LogbookViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(__FUNCTION__)
 //        tableView.backgroundColor = UIColor.redColor()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -20,7 +21,16 @@ class LogbookViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
 
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PostCell", forIndexPath: indexPath) as UITableViewCell
+        
+        return cell
+    }
 
 }
 
