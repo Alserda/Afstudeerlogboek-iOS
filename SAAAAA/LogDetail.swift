eb90600531b4d214f733182b55312cd687d3a47e
Peter Alserda <peteralserda@hotmail.com>
2016-03-07 10:20:37 +0100

    logpost detail page added

---
+++ SAAAAA/LogDetail.swift
@@ -0,0 +1,16 @@
+//
+//  LogDetail.swift
+//  SAAAAA
+//
+//  Created by Peter Alserda on 07/03/16.
+//  Copyright © 2016 Peter Alserda. All rights reserved.
+//
+
+import UIKit
+
+class LogDetail: UIViewController {
+    @IBAction func backButtonPressed(sender: AnyObject) {
+        navigationController?.popViewControllerAnimated(true)
+    }
+}
+
